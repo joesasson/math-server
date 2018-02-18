e1272b0bf37cfeff820f2804083c7cbe759498ca
joesass <dibson2013@gmail.com>
2018-02-18 15:35:30 -0500

    Refactor all handler into one

--- server.go
+++ server.go
@@ -15,6 +15,26 @@ type operands struct {
   // cached bool
 }
 
+func mathHandler(w http.ResponseWriter, r *http.Request){
+  operands := getParams(r)
+  x, y := operands.X, operands.Y
+  answer := 0.0
+  switch operands.Action {
+  case "add":
+    answer = x + y
+  case "subtract":
+    answer = x - y
+  case "multiply":
+    answer = x * y
+  case "divide":
+    answer = x / y
+  }
+  operands.setAnswer(answer)
+  j, _ := json.Marshal(operands)
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(j)
+}
+
 func (o *operands) setAnswer(answer float64){
   o.Answer = answer
 }
@@ -23,43 +43,12 @@ func getParams(r *http.Request) operands {
   params := r.URL.Query()
   x, _ := strconv.ParseFloat(params["x"][0], 64)
   y, _ := strconv.ParseFloat(params["y"][0], 64)
-  action := r.URL.Path
-  fmt.Println(action)
+  action := r.URL.Path[1:]
   return operands{x, y, 0, action}
 }
 
-func addHandler(w http.ResponseWriter, r *http.Request){
-  operands := getParams(r)
-  operands.setAnswer(operands.X + operands.Y)
-  j, _ := json.Marshal(operands)
-  w.Header().Set("Content-Type", "application/json")
-  w.Write(j)
-}
-
-func subtractHandler(w http.ResponseWriter, r *http.Request){
-  operands := getParams(r)
-  answer := operands.X - operands.Y
-  fmt.Fprintln(w, answer)
-}
-
-func multiplyHandler(w http.ResponseWriter, r *http.Request){
-  operands := getParams(r)
-  answer := operands.X * operands.Y
-  fmt.Fprintln(w, answer)
-}
-
-func divideHandler(w http.ResponseWriter, r *http.Request){
-  operands := getParams(r)
-  answer := operands.X / operands.Y
-  fmt.Fprintln(w, answer)
-}
-
-
 
 func main(){
-  http.HandleFunc("/add", addHandler)
-  http.HandleFunc("/subtract", subtractHandler)
-  http.HandleFunc("/multiply", multiplyHandler)
-  http.HandleFunc("/divide", divideHandler)
+  http.HandleFunc("/", mathHandler)
   http.ListenAndServe(":3000", nil)
 }
